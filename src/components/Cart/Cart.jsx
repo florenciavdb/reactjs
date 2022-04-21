2005609e18d0b07ddcba2698de1aca6e99092f52
florenciavdb <florencia.vandenberg@gmail.com>
2022-04-21 17:58:19 -0300

    9.b

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,19 +1,18 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
 import { CartContext } from '../CartContext';
 import r from './Cart.module.css';
 
 const Cart = () => {
-    const {cart, removeItem, buyAll, product } = useContext(CartContext);
+    const {cart, addItem, removeItem } = useContext(CartContext);
 
     return (
         <>
         <h3>CARRITOO!!</h3>
-        <button className={r.Remove} onClick={removeItem}>Remove product</button>
-        <button className={r.Add}>Add product</button>
-        <button className={r.Clear}>Clear Cart</button>
+        
+        
     </> 
-    )
+    )          
 }
+
 
 export default Cart;
--- src/components/CartContext.jsx
+++ src/components/CartContext.jsx
@@ -5,13 +5,24 @@ export const CartContext = createContext ();
 const CartContextProvider = ({ children }) => {
     const [cart, setCart] = useState ([]);
     
-    const addItem = (item) => console.log(item);
+    const addItem = (item) => {
+    const indexProduct = cart.findIndex((cartItem)=>cartItem.id === item.id);
+    if (indexProduct !== -1) {
+        const newCart = [...cart];
+        newCart[indexProduct].count = newCart[indexProduct].count + item.count;
+        setCart(newCart);
+    } else {
+        setCart([...cart, item]);
+    }
+    };
+    console.log(cart);
 
     const removeItem = (id) => {
         setCart([cart.filter((product) => product.id !==id)])
     };
 
     const clear = () => setCart([]);
+    
 
     return (
         <CartContext.Provider value = {{cart, addItem, removeItem, clear}} >
